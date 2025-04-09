09116fb3237d754c07e50185c3bd4c8cdb300e4d
robertpapros <rpapros@nvidia.com>
2025-04-09 10:51:35 +0000

    Add USB-I2C driver support to OCP recovery
    Enhance OCP recovery tool to support devices connected via USB-I2C.
    This improves compatibility with systems where I2C devices are
    reachable through dynamically assigned USB-I2C bridges.
    These changes enable recovery to work with both traditional I2C
    devices and those using USB-I2C drivers.
    
    Unit tests:
    
    1. Perform recovery firmware update when not all GPUs are in
    recovery mode
    
    2. Perform recovery firmware update when at least one GPU is in
    recovery mode
    
    Fixes JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-15912
    
    Signed-off-by: Robert Papros <rpapros@nvidia.com>

--- recovery_tool/ocp_recovery_tool/ocp_commandline.cpp
+++ recovery_tool/ocp_recovery_tool/ocp_commandline.cpp
@@ -2,6 +2,7 @@
 #include "message_registry.hpp"
 #include "recovery_commandline.hpp"
 #include "recoverytool_utils.hpp"
+#include "usb_i2c_mapper.hpp"
 
 #include <CLI/CLI.hpp>
 #include <phosphor-logging/lg2.hpp>
@@ -31,16 +32,89 @@ auto& getBus()
     return bus;
 }
 
-std::pair<uint32_t, uint32_t> getI2CBusAndAddress(const std::string& objPath,
-                                                  const std::string& interface)
+/**
+ * @brief Retrieves the I2C bus number for a given D-Bus object
+ *
+ * This function queries the D-Bus object to get the I2C bus number property
+ * defined in its interface.
+ *
+ * @param[in] objPath - The D-Bus object path to query
+ * @param[in] interface - The interface name containing the I2C bus property
+ * @return uint32_t - The I2C bus number
+ */
+uint32_t getI2CBus(const std::string& objPath, const std::string& interface)
 {
     auto dbusUtil = nvidia::software::updater::DBUSUtils(getBus());
     auto i2cBus = dbusUtil.getProperty<uint64_t>(
         entityManagerService, objPath.c_str(), interface.c_str(), "I2CBus");
+
+    return i2cBus;
+}
+
+/**
+ * @brief Retrieves the I2C address for a given D-Bus object
+ *
+ * This function queries the D-Bus object to get the I2C address property
+ * defined in its interface.
+ *
+ * @param[in] objPath - The D-Bus object path to query
+ * @param[in] interface - The interface name containing the I2C address property
+ * @return uint32_t - The I2C address
+ */
+uint32_t getAddress(const std::string& objPath, const std::string& interface)
+{
+    auto dbusUtil = nvidia::software::updater::DBUSUtils(getBus());
     auto i2cAddress = dbusUtil.getProperty<uint64_t>(
         entityManagerService, objPath.c_str(), interface.c_str(), "I2CAddress");
 
-    return {i2cBus, i2cAddress};
+    return i2cAddress;
+}
+
+/**
+ * @brief Retrieves the USB port for a given D-Bus object
+ *
+ * This function queries the D-Bus object to get the USB port property
+ * defined in its interface.
+ *
+ * @param[in] objPath - The D-Bus object path to query
+ * @param[in] interface - The interface name containing the USB port property
+ * @return std::string - The USB port identifier
+ */
+std::string getUSBPort(const std::string& objPath, const std::string& interface)
+{
+    auto dbusUtil = nvidia::software::updater::DBUSUtils(getBus());
+    auto usbPort = dbusUtil.getProperty<std::string>(
+        entityManagerService, objPath.c_str(), interface.c_str(), "USBPort");
+
+    return usbPort;
+}
+
+/**
+ * @brief Checks if a D-Bus object has a USB port property
+ *
+ * This function queries the D-Bus object to check if it has a USB port property
+ * defined in its interface.
+ *
+ * @param[in] objPath - The D-Bus object path to check
+ * @param[in] interface - The interface name to check for the property
+ * @return bool - True if the object has a USB port property, false otherwise
+ */
+bool hasUSBPortProperty(const std::string& objPath,
+                        const std::string& interface)
+{
+    auto dbusUtil = nvidia::software::updater::DBUSUtils(getBus());
+    try
+    {
+        // Try to get the USB port property - if it exists, return true
+        dbusUtil.getProperty<std::string>(entityManagerService, objPath.c_str(),
+                                          interface.c_str(), "USBPort");
+        return true;
+    }
+    catch (const std::exception& e)
+    {
+        // If we get an exception, the property doesn't exist
+        return false;
+    }
 }
 
 bool performRecovery(const CommandOptions& opts)
@@ -71,9 +145,32 @@ bool performRecovery(const CommandOptions& opts)
 
         lg2::info("Found OCP recovery config Object: {PATH}", "PATH",
                   emObjectPath);
-        const auto [busAddr, slaveAddr] =
-            getI2CBusAndAddress(emObjectPath, ocpObjInterface);
+
         const auto& device = emObjectPath.filename();
+
+        uint32_t busAddr = 0;
+        if (hasUSBPortProperty(emObjectPath, ocpObjInterface))
+        {
+            const auto usbPort = getUSBPort(emObjectPath, ocpObjInterface);
+            const auto i2cBus = recovery_tool::usb_i2c::getI2CBusFromUSBPort(
+                usbPort, opts.verbose);
+
+            if (i2cBus < 0)
+            {
+                lg2::error("Failed to get I2C bus from USB port {USBPORT}",
+                           "USBPORT", usbPort);
+                continue;
+            }
+
+            busAddr = i2cBus;
+        }
+        else
+        {
+            busAddr = getI2CBus(emObjectPath, ocpObjInterface);
+        }
+
+        const auto slaveAddr = getAddress(emObjectPath, ocpObjInterface);
+
         ocp_recovery_commandline::OCPRecoveryCommandLine
             ocpRecoveryCommandlineObj(device, busAddr, slaveAddr, opts.verbose,
                                       opts.emulation);
