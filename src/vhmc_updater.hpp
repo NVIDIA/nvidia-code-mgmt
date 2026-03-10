3cc35d27aab4e1c3a56df2886a790aeae6f55b63
Aayush Tyagi <aayusht@nvidia.com>
2026-03-10 21:56:06 +0530

    Add vHMC non-PLDM firmware update support
    Introduce VHMCItemUpdater for non-PLDM firmware upgrade of the vHMC
    container image. The updater resolves the image upload directory from
    vhmc_config.sh and uses vhmc_stage.sh for staging.
    
    - Add VHMCItemUpdater extending BaseItemUpdater; getImageUploadDir()
      reads VHMC_IMAGE_UPLOAD_DIR from the vHMC config script with RAII
      popen handling and phosphor-logging for errors and fallbacks
    - Add com.Nvidia.vHMC.Updater.service (D-Bus code-manager) and
      vhmc-update@.service invoking vhmc_stage.sh
    - Add VHMC_FLASH_SUPPORT and related meson options
    - Use _HYP_ encoding for hyphens in systemd service args for paths
      containing '-' characters
    - Pass PLDM ComponentVersionString as the third argument field
    
    Change-Id: I0b31e63ece2ee44967406437688120cbfced86e2
    Signed-off-by: Aayush Tyagi <aayusht@nvidia.com>

---
+++ src/vhmc_updater.hpp
@@ -0,0 +1,79 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2024 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+#include "config.h"
+
+#include "base_item_updater.hpp"
+
+namespace nvidia
+{
+namespace software
+{
+namespace updater
+{
+
+class VHMCItemUpdater : public BaseItemUpdater
+{
+  public:
+    VHMCItemUpdater(sdbusplus::bus::bus& bus) :
+        BaseItemUpdater(bus, VHMC_SUPPORTED_MODEL, VHMC_INVENTORY_IFACE, "VHMC",
+                        VHMC_BUSNAME_UPDATER, VHMC_UPDATE_SERVICE, false,
+                        VHMC_BUSNAME_INVENTORY)
+    {}
+
+    std::string getImageUploadDir() const override;
+
+    std::string getVersion(const std::string& inventoryPath) const override;
+    std::string
+        getManufacturer(const std::string& inventoryPath) const override;
+    std::string getModel(const std::string& inventoryPath) const override;
+
+    virtual std::string getServiceArgs(
+        [[maybe_unused]] const std::string& inventoryPath,
+        const std::string& imagePath, const std::string& version,
+        [[maybe_unused]] const TargetFilter& targetFilter) const override
+    {
+        std::string args = inventoryPath;
+        args += "\\x20";
+        args += imagePath;
+        args += "\\x20";
+        args += version;
+        std::string escaped;
+        for (char c : args)
+        {
+            if (c == '-')
+            {
+                escaped += "_HYP_";
+            }
+            else if (c == '/')
+            {
+                escaped += '-';
+            }
+            else
+            {
+                escaped += c;
+            }
+        }
+        return escaped;
+    }
+
+    std::vector<std::string> getItemUpdaterInventoryPaths() override
+    {
+        std::vector<std::string> ret;
+        std::string invPath = std::string(SOFTWARE_OBJPATH) + "/VHMC";
+        ret.emplace_back(invPath);
+        return ret;
+    }
+
+    bool inventorySupported() override
+    {
+        return false;
+    }
+};
+
+} // namespace updater
+} // namespace software
+} // namespace nvidia
